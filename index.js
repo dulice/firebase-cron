d685cf8bcddea19078d8994a828fcfb70f838168
dulice <duliceellen600@gmail.com>
2025-01-10 15:20:08 +0530

    vercel

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ dotenv.config();
 import express from "express";
 import corn from "node-cron";
 import cors from "cors";
-import { drawLottery } from "./firebase-admin.js";
+import { createWinner, drawLottery } from "./firebase-admin.js";
 import { lotteryTime } from "./constants.js";
 
 const app = express();
@@ -12,18 +12,31 @@ app.use(cors());
 app.use(express.json());
 
 corn.schedule("0 12 28-31 * *", async () => {
-  const { lotteryDate, today } = lotteryTime();
-  if (lotteryDate.toLocaleString() === today.toLocaleString()) {
-    console.log("running on serverless");
-    await drawLottery();
-    console.log("running on schedule");
-  } else {
-    console.log("Today is not the last day of the month. Skipping...");
+  try {
+    const { lotteryDate, today } = lotteryTime();
+    if (lotteryDate.toLocaleString() === today.toLocaleString()) {
+      console.log("running on serverless");
+      await drawLottery();
+      console.log("running on schedule");
+    } else {
+      console.log("Today is not the last day of the month. Skipping...");
+    }
+  } catch (error) {
+    console.log(error);
   }
 });
 
-corn.schedule("* * * * *", () => {
-  console.log("running every minute");
+corn.schedule("* * * * *", async () => {
+  try {
+    console.log("running every minute");
+    await createWinner({
+      number: "99",
+      created_at: Date.now(),
+      users: [],
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.use("*", (req, res) => {
